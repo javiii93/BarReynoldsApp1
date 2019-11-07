3ea14553f7a14a97804b8bfc0dd0e1bfada27124
adriangc24 <adriangcamacho24@gmail.com>
2019-11-08 00:25:21 +0100

    Xpath is querying nice

--- app/src/main/java/com/example/barreynoldsapp1/CategoriasActivity.java
+++ app/src/main/java/com/example/barreynoldsapp1/CategoriasActivity.java
@@ -5,11 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.AssetManager;
+import android.content.res.Resources;
+import android.content.res.XmlResourceParser;
+import android.graphics.drawable.Drawable;
 import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
+import android.widget.ImageSwitcher;
 import android.widget.ImageView;
 import android.widget.ListView;
 
@@ -17,31 +21,44 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 
 public class CategoriasActivity extends AppCompatActivity {
     private String categoria;
     private ListView lista;
     private ArrayList<Producto> arrayProductos = new ArrayList<>();
     private ArrayAdapter<Producto> adaptador;
-    private int[]flags;
+    private String imgUri;
+    Resources resources;
+    XmlResourceParser xmlParser;
+    Document doc;
+    Producto p1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categorias);
 
         categoria = getIntent().getStringExtra("categoria");
+        resources = getResources();
 
-        flags = new int[]{
-          R.drawable.aquarius
-        };
 
         // Instanciamos objetos Clase R
         lista = findViewById(R.id.listView);
@@ -58,30 +75,54 @@ public class CategoriasActivity extends AppCompatActivity {
             InputStream istream = getAssets().open("productes.xml");
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(istream);
-            NodeList nl =doc.getElementsByTagName(categoria);
-            Element e=(Element)nl.item(0);
+            doc = dBuilder.parse(istream);
+            // NodeList de la categoria
+            NodeList nl = doc.getElementsByTagName(categoria);
+            // Element del primer elemento del NodeList
+            Element e = (Element) nl.item(0);
+            // NodeList de los hijos del elemento
             NodeList nl2 = e.getChildNodes();
 
             for (int i = 0; i < nl2.getLength(); i++) {
                 if (nl2.item(i).getNodeType() == Node.ELEMENT_NODE) {
                     Element el = (Element) nl2.item(i);
-                    Producto p1 = new Producto();
+                    p1 = new Producto();
+
                     p1.setNombre(el.getElementsByTagName("nom").item(0).getTextContent());
                     p1.setPrecio(Float.parseFloat(el.getElementsByTagName("preu").item(0).getTextContent()));
                     p1.setDescripcion(el.getElementsByTagName("descripcio").item(0).getTextContent());
-                    //p1.setCantidad(Integer.parseInt(el.getElementsByTagName("preu").item(0).getTextContent()));
                     p1.setCategoria(categoria);
-
-
+                    //p1.setCantidad(Integer.parseInt(el.getElementsByTagName("preu").item(0).getTextContent()));
                     arrayProductos.add(p1);
-                }
 
+                }
             }
+            // Rellenar imagenes
+            queryXpath(categoria);
 
         } catch (Exception e) {
             e.printStackTrace();
+
         }
     }
+    public void queryXpath(String categoria) throws XPathExpressionException {
+        XPath xPath = XPathFactory.newInstance().newXPath();
+        NodeList nodes = (NodeList) xPath.evaluate("//"+categoria+"/producte/imatge/image/@href", doc, XPathConstants.NODESET);
 
+        for (int x = 0; x < nodes.getLength(); x++) {
+            Node node = nodes.item(x);
+            try {
+                // get input stream
+                InputStream ims = getAssets().open(node.getTextContent());
+                // load image as Drawable
+                Drawable d = Drawable.createFromStream(ims, null);
+                // set image to ImageView
+                //p1.setImagen();
+                System.out.println(node.getTextContent());
+            }
+            catch(IOException ex) {
+                return;
+            }
+        }
+    }
 }
