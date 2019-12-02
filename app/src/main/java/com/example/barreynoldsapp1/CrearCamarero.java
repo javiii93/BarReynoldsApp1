fa87664800d5de299a4187d804c610c7433d58a8
adriangc24 <adriangcamacho24@gmail.com>
2019-12-02 17:45:54 +0100

    adding crearCamarero foto

--- app/src/main/java/com/example/barreynoldsapp1/CrearCamarero.java
+++ app/src/main/java/com/example/barreynoldsapp1/CrearCamarero.java
@@ -2,13 +2,26 @@ package com.example.barreynoldsapp1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 
 public class CrearCamarero extends AppCompatActivity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_camarero);
+
+
+
+    }
+    public void dispatchTakePictureIntent(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
 }
